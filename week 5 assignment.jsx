98a64959dd2a4ee9625a6fd04469a804eba2ea5e
Adebisi Racheal Ajibola <mojibolarachael@gmail.com>
2025-08-28 08:16:02 -0700

    Add files via upload

---
+++ week 5 assignment.jsx
@@ -0,0 +1,52 @@
+statements
+let name = "Racheal";
+
+// 2. case-sensitive
+let Name = "Racheal";// Different variable from 'name'
+
+// 3. code block { }
+
+// 4. comments
+// This is a single-line comment
+
+/* This is a 
+
+
+
+multi-line comment */
+
+// VARIABLES:
+// 1. Using the let keyword
+let age = 25;
+age = 27;
+
+// 2. using the const keyword
+const birthYear = 1998;
+// birthYear = 2000; // Error: Assignment to constant variable.
+
+
+// Data Types:
+// 1. Primitive Data Types (basic building blocks)
+let city = "New York"; // string
+let temperature = 72.5; // Number
+let isSunny = true; // Boolean
+let address; // Undefined
+let middleName = null; // Null, VS 0 (zero)
+
+// 2. Non-Primitive Data Types (complex structures)
+// Object:
+let person = { firstName: "Racheal", lastName: "Adebisi" }; // key value pairs
+// Array:
+let colors = ["red", "green", "blue"]; // ordered list 
+// Function:
+function greet() { console.log("Hello!"); } // reusable block of code
+// Date:
+let today = new Date(); // represents a specific date and time
+// RegExp:
+let pattern = /[a-z]/; // regular expression for matching patterns
+// Map:
+let scores = new Map(); // collection of key-value pairs
+// Set:
+let uniqueNumbers = new Set([1, 2, 3, 4, 4]); // collection of unique values
+    
+
